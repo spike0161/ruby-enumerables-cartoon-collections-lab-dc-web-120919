e6bfa5b35f61bc0c9f31e1c7456397ea76cda9da
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-10-17 13:59:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
 
-dwarves.each_with_index {|item, index| puts "#{index +1} #{item}"}
+dwarves.each_with_index {|potato, index| puts "#{index +1} #{potato}"}
 
 end
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
